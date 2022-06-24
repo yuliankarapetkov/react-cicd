ed6e674ada92824d951db1aa1bf93efef87d3431
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2022-06-24 14:38:42 +0300

    fix: update config

--- release.config.js
+++ release.config.js
@@ -5,6 +5,12 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"]
+      }
+    ],
     "@semantic-release/github"
   ]
 };
